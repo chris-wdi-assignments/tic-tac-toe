5725fef3a038044458217850fb0d745c091cf7a4
Christopher Fujino <christopherfujino@gmail.com>
2017-07-04 10:50:08 -0700

    added: onclick event handler to the board

--- js/app.js
+++ js/app.js
@@ -2,5 +2,8 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
-
+  $('#board').on('click', function (e) {
+    console.log(e.target);
+    $(e.target).text('O');
+  });
 });
