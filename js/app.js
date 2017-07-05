4b7c257c3aa55ea81ac5adfbe6894b9902855c74
Christopher Fujino <christopherfujino@gmail.com>
2017-07-04 18:32:32 -0700

    added: reset logic

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
+const board = $('#board');
+
+const turn = 'x'; // x always goes first
+
+const reset = () => {
+  turn = 'x'; // x goes first
+  board.children('.square').each(() => $(this).text(''));
+};
+
 // wait for the DOM to finish loading
+
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
   $('#board').on('click', function (e) {
-    console.log(e.target);
-    $(e.target).text('O');
+    play(e.target);
   });
 });
