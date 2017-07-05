5d791c7347c44a42157eea40cd92ce86bf15af8c
Christopher Fujino <christopherfujino@gmail.com>
2017-07-04 19:25:36 -0700

    added: play logic

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 const board = $('#board');
 
-const turn = 'x'; // x always goes first
+let turn = 'x'; // x always goes first
 
 const reset = () => {
   turn = 'x'; // x goes first
   board.children('.square').each(() => $(this).text(''));
+};
+
+const play = (el) => {
+  $el = $(el);  // just query the element once
+  contents = $el.text();
+  if (contents === 'x' || contents === 'o') return null;
+  $el.text(turn);
+  if (turn === 'x') turn = 'o';
+  else if (turn === 'o') turn = 'x';
+  else throw new Error(`turn is ${turn}, not known!`);
 };
 
 // wait for the DOM to finish loading
